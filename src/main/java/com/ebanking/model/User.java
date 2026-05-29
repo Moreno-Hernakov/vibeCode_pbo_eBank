57e9bfd6405e6705521266a8f145fc7347256991
Moreno Hernakov <mhadn29@gmail.com>
2026-05-29 20:33:06 +0700

    refactor: update User model

--- src/main/java/com/ebanking/model/User.java
+++ src/main/java/com/ebanking/model/User.java
@@ -26,7 +26,10 @@ public class User {
     public void setUsername(String username) { this.username = username; }
 
     public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    
+    public void setPassword(String password) { 
+        this.password = password;
+    }
 
     public String getCifNumber() { return cifNumber; }
     public void setCifNumber(String cifNumber) { this.cifNumber = cifNumber; }
